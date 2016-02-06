92f7723e2a43f7423c97d8987255a82dabf35222
Nathan Reed <reednj@gmail.com>
2016-02-06 19:19:26 +0800

    update the deploy script to back up the data and update the version.txt

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ WEB=~/www/twtxt.reednj.com
 SRC=~/code/twtxt.git
 CONFIG=~/code/config_backup/twtxt
 
+# update the version file
+git --git-dir=$SRC describe --long --always --abbrev=3 > version.txt
+
+# update the data
+mkdir $SRC/data
+cp $WEB/data/* $SRC/data
+
 # copy the required files to the website
 rm -rf $WEB/*
 cp -R $SRC/* $WEB
