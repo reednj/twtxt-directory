d07967990199adf16ce75e58faa6160265ed8930
Nathan Reed <reednj@gmail.com>
2016-02-10 18:00:06 +0800

    enhance update-user code to remove inactive users

--- sh/update-user.rb
+++ sh/update-user.rb
@@ -18,6 +18,15 @@ class App
 		UserHelper.update_user(user)
 
 		puts "@#{user.username} updated"
+
+		# now we refresh the data in the user, and check if they have any updates
+		# users that still have no updates 3 days after they were created get deleted
+		user.refresh
+		if user.update_count == 0 && user.created_date.age > 3.days
+			user.delete
+			puts "@#{user.username} deleted - no updates for 3 days"
+		end
+
 	end
 
 	def user_to_update
